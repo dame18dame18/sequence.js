53a903377036930b60ae08857daf1e7a7690694e
William Hua <william@attente.ca>
2022-12-22 18:22:35 -0500

    account: use status.version guest module

--- packages/account/src/account.ts
+++ packages/account/src/account.ts
@@ -478,7 +478,7 @@ export class Account {
 
     // Everything is encoded as a bundle
     // using the GuestModule of the account version
-    const { guestModule } = this.contextFor(this.version)
+    const { guestModule } = this.contextFor(status.version)
     return { entrypoint: guestModule, transactions }
   }
 
